e53d8171a79ab616b78c1879f0e9e8aa0916bd0b
Ruchir Shukla <ruchir@bizzappdev.com>
2016-12-08 12:15:18 +0530

    added file

---
+++ report.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+from openerp import SUPERUSER_ID
+from openerp.exceptions import AccessError
+from openerp.osv import osv, fields
+from openerp.sql_db import TestCursor
+from openerp.tools import config
+from openerp.tools.misc import find_in_path
+from openerp.tools.translate import _
+from openerp.addons.web.http import request
+from openerp.tools.safe_eval import safe_eval as eval
+from openerp.exceptions import UserError
+
+import re
+import time
+import base64
+import logging
+import tempfile
+import lxml.html
+import os
+import subprocess
+from contextlib import closing
+from distutils.version import LooseVersion
+from functools import partial
+from pyPdf import PdfFileWriter, PdfFileReader
+from reportlab.graphics.barcode import createBarcodeDrawing
+
+class Report(models.Model):
+    _inherit = "report"
+
+
+    @api.v7
+    def get_pdf(self, cr, uid, ids, report_name, html=None, data=None, context=None):
+        """This method generates and returns pdf version of a report.
+        """
+        print "22222222222222222222222222", report_name, ids, context
+        if context is None:
+            context = {}
+
+        # As the assets are generated during the same transaction as the rendering of the
+        # templates calling them, there is a scenario where the assets are unreachable: when
+        # you make a request to read the assets while the transaction creating them is not done.
+        # Indeed, when you make an asset request, the controller has to read the `ir.attachment`
+        # table.
+        # This scenario happens when you want to print a PDF report for the first time, as the
+        # assets are not in cache and must be generated. To workaround this issue, we manually
+        # commit the writes in the `ir.attachment` table. It is done thanks to a key in the context.
+        if not config['test_enable']:
+            context = dict(context, commit_assetsbundle=True)
+
+        if html is None:
+            html = self.get_html(cr, uid, ids, report_name, data=data, context=context)
+
+        # The test cursor prevents the use of another environnment while the current
+        # transaction is not finished, leading to a deadlock when the report requests
+        # an asset bundle during the execution of test scenarios. In this case, return
+        # the html version.
+        if isinstance(cr, TestCursor):
+            return html
+
+        html = html.decode('utf-8')  # Ensure the current document is utf-8 encoded.
+
+        # Get the ir.actions.report.xml record we are working on.
+        report = self._get_report_from_name(cr, uid, report_name)
+        print "22211111111111111111!", report
+        # Check if we have to save the report or if we have to get one from the db.
+        save_in_attachment = self._check_attachment_use(cr, uid, ids, report)
+        # Get the paperformat associated to the report, otherwise fallback on the company one.
+        if not report.paperformat_id:
+            user = self.pool['res.users'].browse(cr, uid, uid)
+            paperformat = user.company_id.paperformat_id
+        else:
+            paperformat = report.paperformat_id
+
+        # Preparing the minimal html pages
+        headerhtml = []
+        contenthtml = []
+        footerhtml = []
+        irconfig_obj = self.pool['ir.config_parameter']
+        base_url = irconfig_obj.get_param(cr, SUPERUSER_ID, 'report.url') or irconfig_obj.get_param(cr, SUPERUSER_ID, 'web.base.url')
+
+        # Minimal page renderer
+        view_obj = self.pool['ir.ui.view']
+        render_minimal = partial(view_obj.render, cr, uid, 'report.minimal_layout', context=context)
+
+        # The received html report must be simplified. We convert it in a xml tree
+        # in order to extract headers, bodies and footers.
+        try:
+            root = lxml.html.fromstring(html)
+            match_klass = "//div[contains(concat(' ', normalize-space(@class), ' '), ' {} ')]"
+
+            for node in root.xpath(match_klass.format('header')):
+                body = lxml.html.tostring(node)
+                header = render_minimal(dict(subst=True, body=body, base_url=base_url))
+                headerhtml.append(header)
+
+            for node in root.xpath(match_klass.format('footer')):
+                body = lxml.html.tostring(node)
+                footer = render_minimal(dict(subst=True, body=body, base_url=base_url))
+                footerhtml.append(footer)
+
+            for node in root.xpath(match_klass.format('page')):
+                # Previously, we marked some reports to be saved in attachment via their ids, so we
+                # must set a relation between report ids and report's content. We use the QWeb
+                # branding in order to do so: searching after a node having a data-oe-model
+                # attribute with the value of the current report model and read its oe-id attribute
+                if ids and len(ids) == 1:
+                    reportid = ids[0]
+                else:
+                    oemodelnode = node.find(".//*[@data-oe-model='%s']" % report.model)
+                    if oemodelnode is not None:
+                        reportid = oemodelnode.get('data-oe-id')
+                        if reportid:
+                            reportid = int(reportid)
+                    else:
+                        reportid = False
+
+                # Extract the body
+                body = lxml.html.tostring(node)
+                reportcontent = render_minimal(dict(subst=False, body=body, base_url=base_url))
+
+                contenthtml.append(tuple([reportid, reportcontent]))
+
+        except lxml.etree.XMLSyntaxError:
+            contenthtml = []
+            contenthtml.append(html)
+            save_in_attachment = {}  # Don't save this potentially malformed document
+
+        # Get paperformat arguments set in the root html tag. They are prioritized over
+        # paperformat-record arguments.
+        specific_paperformat_args = {}
+        for attribute in root.items():
+            if attribute[0].startswith('data-report-'):
+                specific_paperformat_args[attribute[0]] = attribute[1]
+
+        # Run wkhtmltopdf process
+        return self._run_wkhtmltopdf(
+            cr, uid, headerhtml, footerhtml, contenthtml, context.get('landscape'),
+            paperformat, specific_paperformat_args, save_in_attachment,
+            context.get('set_viewport_size')
+        )
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
