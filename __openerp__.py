021c5113b27ad847c6bb54ed8e496643d36d7cb7
Ruchir Shukla <ruchir@bizzappdev.com>
2016-12-08 11:10:41 +0530

    added basic module

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Customer Background",
+    "version": "0.1",
+    "author": "BizzAppDev",
+    "website": "http://www.bizzappdev.com",
+    "category": "GenericModules",
+    "depends": ["base", 'report'],
+    "summary": "Customer Background",
+    "description": """
+    """,
+    'images': [],
+    "init_xml": [],
+    "data": [
+    ],
+    'demo_xml': [
+    ],
+    'test':[
+    ],
+    'installable': True,
+    'active': False,
+    'auto_install': False,
+    'application':False
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
