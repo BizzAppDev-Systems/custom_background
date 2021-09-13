87b65ecbb376373193a2a07cf3932087088aeaef
RUS <rus@bizzappdev.com>
2021-09-13 18:08:02 +0530

    [UP]dump version

--- custom_background/__manifest__.py
+++ custom_background/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Custom Background",
-    "version": "14.0.0.0.8",
+    "version": "14.0.0.0.9",
     "author": "BizzAppDev",
     "website": "http://www.bizzappdev.com",
     "category": "GenericModules",
