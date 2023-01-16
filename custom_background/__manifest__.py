6320e5443a2ac2014a12eba7b0e50ac151d58731
RUS <RUS@bizzappdev.com>
2023-01-16 13:03:20 +0530

    VERSION CHANGE

--- custom_background/__manifest__.py
+++ custom_background/__manifest__.py
@@ -1,7 +1,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     "name": "Custom Background",
-    "version": "16.0.0.0.2",
+    "version": "16.0.0.0.3",
     "author": "BizzAppDev",
     "website": "http://www.bizzappdev.com",
     "category": "GenericModules",
