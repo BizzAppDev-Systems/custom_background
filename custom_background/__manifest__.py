7dec61e4ce20df5fa8e147e53e5d9a07871f454a
[DHR] Dhaval Rana <dhaval.rana@bizzappdev.com>
2024-04-29 07:43:27 +0000

    [IMP][T7743] custom_background: Update the license file and license in manifest -- DHR

--- custom_background/__manifest__.py
+++ custom_background/__manifest__.py
@@ -1,7 +1,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     "name": "Custom Background",
-    "version": "15.0.0.0.1",
+    "version": "15.0.0.0.2",
     "author": "BizzAppDev",
     "website": "http://www.bizzappdev.com",
     "category": "GenericModules",
@@ -21,5 +21,5 @@
             "/custom_background/static/src/scss/report_qweb_pdf.scss",
         ],
     },
-    "license": "Other proprietary",
+    "license": "AGPL-3",
 }
