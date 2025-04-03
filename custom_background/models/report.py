de75b778e51c4f58885cd4babcb9875c6e1678cf
APA <aayushi.panchal@bizzappdev.com>
2025-04-03 17:59:27 +0530

    [IMP][T9428] custom_background:Remove commnet.

--- custom_background/models/report.py
+++ custom_background/models/report.py
@@ -628,7 +628,6 @@ class IrActionsReport(models.Model):
             in ["dynamic", "dynamic_per_report_company_lang"]
         ):
             if report.custom_report_type == "dynamic_per_report_company_lang":
-                # search append attachment record. #T6622
                 company = self._context.get("background_company")
                 # Filter append attachments for the current report and company #T9428
                 append_attachment = report.per_report_com_lang_bg_ids.filtered(
